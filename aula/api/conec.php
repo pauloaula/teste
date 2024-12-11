62035eef4d59e9c9f674c04df900103275c1b8e0
pauloaula <paulo.abd3@hotmail.com>
2024-12-11 19:24:28 -0300

    Update conec.php

--- aula/api/conec.php
+++ aula/api/conec.php
@@ -1,8 +1,8 @@
 <?php 
-$localdb="sql10.freemysqlhosting.net";
-$userdb="sql10750804";
-$senhadb="5xsihG3t4Q";
-$nomedb="sql10750804";
+$localdb="mysql-dbproz.alwaysdata.net";
+$userdb="dbproz";
+$senhadb="pauloandre123";
+$nomedb="dbproz_aula";
 $port="3306";
 $conn = mysqli_connect($localdb,$userdb,$senhadb,$nomedb,$port);
 
